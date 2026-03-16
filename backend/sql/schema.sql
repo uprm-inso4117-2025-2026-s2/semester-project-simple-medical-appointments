608a39d50bf87d73424e4785665e7185bce43150
Jonathan-Rod <60677254+Jonathan-Rod@users.noreply.github.com>
2026-03-15 20:07:42 -0400

    Add user_roles table to user management module schema

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -8,7 +8,18 @@ CREATE TABLE roles (
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
 );
 
--- TODO: user_roles
+CREATE TABLE user_roles (
+    user_id UUID NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES auth.users (id) ON DELETE CASCADE,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE,
+    granted_by UUID NULL,
+    FOREIGN KEY (granted_by) REFERENCES auth.users (id) ON DELETE SET NULL,
+    PRIMARY KEY (user_id, role_id),
+    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
 -- TODO: permissions
 -- TODO: role_permissions
 -- TODO: profiles
