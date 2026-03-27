47cf51e9469d4babeb9d532a1dcb14df871a8af1
Jonathan-Rod <60677254+Jonathan-Rod@users.noreply.github.com>
2026-03-27 18:18:33 -0400

    Implement get_user_roles function to retrieve user roles from the database

--- backend/custom_decorators.py
+++ backend/custom_decorators.py
@@ -1,1 +1,24 @@
-# TODO Add requires_role decorator
+import sqlite3
+
+
+def get_user_roles(user_id: int, db: sqlite3.Connection) -> set[str]:
+    """ Returns the roles of a user
+
+    Args:
+        user_id (int): user id
+        db (sqlite3.Connection): database connection
+
+    Returns:
+        set[str]: set of roles
+    """
+    cursor = db.cursor()
+    query = """
+            SELECT roles.name FROM user_roles
+            JOIN roles ON user_roles.role_id = roles.id
+            WHERE user_roles.user_id = ?
+            """
+    cursor.execute(query, (user_id,))
+    rows = cursor.fetchall()
+
+    role_names = {row[0] for row in rows}
+    return role_names
